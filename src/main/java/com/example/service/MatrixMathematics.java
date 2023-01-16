e3a0accb13aa90091864dabcf3078fba7b19cb22
baziz-meriem <74902350+baziz-meriem@users.noreply.github.com>
2023-01-16 09:38:00 +0100

    Update MatrixMathematics.java

--- src/main/java/com/example/service/MatrixMathematics.java
+++ src/main/java/com/example/service/MatrixMathematics.java
@@ -116,8 +116,10 @@ public class MatrixMathematics {
 	 */
 		public static Matrix inverse(Matrix matrix) throws NoSquareException {
 			double d= determinant(matrix);
-		if(d==0){return null;} else
-		return (transpose(cofactor(matrix)).multiplyByConstant(1.0/determinant(matrix)));
+		if(d==0){return null;} else{
+			return (transpose(cofactor(matrix)).multiplyByConstant(1.0/determinant(matrix)));
+		}
+		
 	}
 
 
